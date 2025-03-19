1dd145f675f5f43901b32671d313f020d1171600
jackoup <yusuf.fakhry341@gmail.com>
2025-03-19 16:37:41 +0200

    initial commit and created user model for ORM

---
+++ models/user.py
@@ -0,0 +1,15 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)

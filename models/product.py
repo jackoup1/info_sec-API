96483f93f5b7e0bca3d9c3592850a1d52714a1a8
jackoup <yusuf.fakhry341@gmail.com>
2025-03-19 16:39:21 +0200

    created product.py model

---
+++ models/product.py
@@ -0,0 +1,11 @@
+from . import db
+from datetime import datetime
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    pid = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)

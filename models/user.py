407fb22d530db58e5c24988bd0009932168b82ba
jackoup <yusuf.fakhry341@gmail.com>
2025-03-20 23:25:41 +0200

    fixed password saved with lenght of 100 charcther which caused an error since the hash is 256 char

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
